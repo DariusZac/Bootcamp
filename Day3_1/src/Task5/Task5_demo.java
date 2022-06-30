268c3f1ad06615faac266f03bf4e0f8dd6a69cf2
DariusZac <zat4521@gmail.com>
2022-06-30 13:59:35 +0300

    Task 5 of day 3

--- Day3_1/src/Task5/Task5_demo.java
+++ Day3_1/src/Task5/Task5_demo.java
@@ -1,5 +1,17 @@
 package Task5;
 
+
+//Vehicle
+// Create a base class of a Vehicle,
+// then create a Car class which inherits from the Vehicle class.
+// Also, create another class, a specific type of Car that inherits
+// from the Car class.
+// Program should be able to handle steering, changing gears
+// and drive (having speed in other words).
+// You might decide where to put the appropriate state and
+// behaviors (fields and methods).
+// As mentioned above, changing gears, increasing/decreasing speed
+// of driving should be included.
 public class Task5_demo {
     public static void main(String[] args) {
         Cabriolet car1 = new Cabriolet();
